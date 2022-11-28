3e2265fe9ee60e4693a4b812dad2da88888fa9c7
NandaSelvi <114274096+NandaSelvi@users.noreply.github.com>
2022-11-28 20:47:40 +0700

    update

--- src/pages/not-found.test.js
+++ src/pages/not-found.test.js
@@ -1,5 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import NotFound from './not-found';
+import Container from '@mui/material/Countainer';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 test('File Not Found 404', () => {
     render(<NotFound />);
