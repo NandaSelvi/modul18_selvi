13792654216681c4597788c1932c57a11179a5c3
NandaSelvi <114274096+NandaSelvi@users.noreply.github.com>
2022-12-06 10:12:24 +0700

    ...

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -49,8 +49,6 @@ const Profile = () => {
               token : token })
             if(response.status === 200){
               setIsLogin(true)
-            }else{
-              navigate('/login')
             }
             
           } catch (error) {
