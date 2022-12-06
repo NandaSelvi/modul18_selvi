bf9f67a2895158232a3a0e22b318a8b77240e891
NandaSelvi <114274096+NandaSelvi@users.noreply.github.com>
2022-12-06 09:35:36 +0700

    update

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -48,7 +48,7 @@ const Profile = () => {
         // 2. buat fungsi verifikasi token yang sama seperti di halaman home
         const verify = async() =>{
           try {
-            const response = await axios.post('http://localhost:9000/verify', {
+            const response = await axios.post('https://modul17selvi-production.up.railway.app/verify', {
               token: localStorage.getItem('token')
             })
             if(response.status == 200){
@@ -86,7 +86,7 @@ const Profile = () => {
 
         // 2. Hit endpoint logout dengan body jwt yang didapat dari localstorage
         //   dan setelah berhasil, beri alert sukses
-        await axios.post('http://localhost:9000/logout', {
+        await axios.post('https://modul17selvi-production.up.railway.app/logout', {
             jwt: localStorage.getItem('token')
         })
         .then((res) => {
